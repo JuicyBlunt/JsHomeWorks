fccf5aac4da49cba2a31140168b52681d9498a48
Name <myEmail@example.com>
2021-07-27 20:19:38 +0300

    first task

---
+++ main.js
@@ -0,0 +1,9 @@
+//=====1======//
+for (let i = 1; i <= 10; i++) {
+  i % 3 == 0
+    ? console.log("FizBuz")
+    : i % 2 == 0
+    ? console.log("Fiz")
+    : console.log("Buz");
+}
+//=====2======//
