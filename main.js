62b2556f9a3151dceea0fcb215327ee55c7009d2
Name <myEmail@example.com>
2021-07-27 23:23:31 +0300

    complete 4 tasks

--- main.js
+++ main.js
@@ -6,4 +6,47 @@ for (let i = 1; i <= 10; i++) {
     ? console.log("Fiz")
     : console.log("Buz");
 }
+
 //=====2======//
+function factorial(num) {
+  return num == 1 ? num : num * factorial(num - 1);
+}
+
+//=====3======//
+const sheetsInReamPaper = 500;
+const consumptionPerWeek = 1200;
+const weeksAmount = 8;
+
+function enoughtPaper() {
+  let amountPerWeeks = (weeksAmount * consumptionPerWeek) / sheetsInReamPaper;
+  return amountPerWeeks % 1 == 0
+    ? amountPerWeeks
+    : amountPerWeeks - (amountPerWeeks % 1) + 1;
+}
+console.log(enoughtPaper());
+//=====4======//
+const roomsOnFloor = 3;
+const floors = 9;
+const roomNumber = 456;
+let porch;
+let floor;
+function findCustomer() {
+  let entrance = roomNumber / (floors * roomsOnFloor);
+  porch = entrance % 1 == 0 ? entrance : entrance - (entrance % 1) + 1;
+  let counter = 0;
+  for (let i = 1; i <= porch; i++) {
+    for (let j = 1; j <= floors; j++) {
+      for (let k = 1; k <= roomsOnFloor; k++) {
+        counter++;
+        if (counter == roomNumber) {
+          floor = j;
+        }
+      }
+    }
+  }
+}
+findCustomer();
+console.log(porch, floor);
+
+//=====5======//
+const medianNumber = 6;
