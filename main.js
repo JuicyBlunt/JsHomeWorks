339c8987ae7ed2ada0821d503d929994760e59cb
Name <myEmail@example.com>
2021-07-31 14:13:41 +0300

    done 4 tasks

--- main.js
+++ main.js
@@ -12,4 +12,63 @@ const result = Object.entries(citiesAndCountries).map((item) =>
   item.join(" - это ")
 );
 console.log(result);
+
 //======2======//
+function getArray() {
+  const amount = 9;
+  let counter = 0;
+  let arr = [];
+  if (amount % 3 == 0) {
+    for (let i = 0; i < amount / 3; i++) {
+      arr[i] = [];
+      for (let j = 1; j <= 3; j++) {
+        counter++;
+        arr[i].push(counter);
+      }
+    }
+    return arr;
+  }
+  console.error("Invalid number");
+}
+console.log(getArray());
+
+//======3======//
+const namesOfDays = {
+  ru: [
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+    "Воскресенье",
+  ],
+  en: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+};
+function getNameOfDay() {
+  const lang = "ru";
+  const day = 3;
+  return namesOfDays[lang][day - 1];
+}
+
+console.log(getNameOfDay());
+
+//======4======//
+const arr = [19, 5, 42, 2, 77];
+function sumFewestNums(arr) {
+  return arr
+    .sort((a, b) => a - b)
+    .slice(0, 2)
+    .reduce((acc, curr) => acc + curr);
+}
+console.log(sumFewestNums(arr));
+
+//======5======//
