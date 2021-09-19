86b948fbfbf8e18785fd9d72f3d06a6b4982031d
Name <myEmail@example.com>
2021-09-19 17:16:49 +0300

    module is done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const events = [
   { start: 0, duration: 15, title: "Exercise" },
-  { start: 25, duration: 30, title: "Travel to work" },
+  { start: 25, duration: 90, title: "Travel to work" },
   { start: 30, duration: 30, title: "Plan day" },
   { start: 60, duration: 15, title: "Review yesterday's commits" },
   { start: 100, duration: 15, title: "Code review" },
@@ -13,7 +13,13 @@ const events = [
 const start = 8;
 const end = 17;
 
-function initCalendar(from = 8, to = 17) {
+function addPropsToArr() {
+  events.forEach((elem) => {
+    elem.bgc = "#e2ecf5";
+  });
+}
+
+function initCalendar(from, to) {
   let wrapper = document.querySelector(".calendar__wrapper");
   let startHour = from * 60;
   let countOfHours = to - from;
@@ -34,69 +40,193 @@ function initCalendar(from = 8, to = 17) {
 
 function initEvents(events) {
   document.forms.delete.remove.innerHTML = "";
+  document.forms.change.replace.innerHTML = "";
   let wrapper = Array.from(document.querySelectorAll(".event__items"));
   wrapper.forEach((item) => (item.innerHTML = ""));
-  events.forEach(({ start, duration, title, bgc }, index) => {
+  events.forEach((event, index) => {
     let searchElement = wrapper.find((item) => {
-      if (+item.dataset.count === Math.trunc(start / 60)) {
+      if (+item.dataset.count === Math.trunc(event.start / 60)) {
         return item;
       }
     });
-    let div = document.createElement("div");
-    div.className = "event__item";
-    div.textContent = title;
-    div.style.height = `${duration * 2}px`;
-    div.style.top = `${(start % 60) * 2}px`;
-    div.style.backgroundColor = bgc || "";
-    searchElement.append(div);
-    //INIT OPTIONS
-    document.forms.delete.remove.insertAdjacentHTML(
-      "beforeend",
-      `<option value=${index}>${title}</option>`
-    );
+    addOneEvent(event, searchElement);
+    //INIT DELETE/CHANGE OPTIONS
+    formOptions(index, event.title);
   });
+}
+
+function addOneEvent({ start, duration, title, bgc }, place) {
+  let hexToRgba = bgc
+    .match(/\w\w/g)
+    .map((x) => parseInt(x, 16))
+    .join();
+  let div = document.createElement("div");
+  div.className = "event__item";
+  div.textContent = title || "No text";
+  div.style.height = `${duration * 2}px`;
+  div.style.top = `${(start % 60) * 2}px`;
+  div.style.backgroundColor = `rgba(${hexToRgba},.5)`;
+  div.style.borderLeftColor = `rgba(${hexToRgba})`;
+  place.append(div);
+}
+
+function formOptions(index, title) {
+  document.forms.delete.remove.insertAdjacentHTML(
+    "beforeend",
+    `<option value=${index}>${title}</option>`
+  );
+  document.forms.change.replace.insertAdjacentHTML(
+    "beforeend",
+    `<option value=${index}>${title}</option>`
+  );
 }
 
 function formsHandler(e) {
   e.preventDefault();
   if (e.target.name === "add") {
-    let text = document.querySelector("#event-title").value;
-    let startTime = document.querySelector("#timeStart").value;
-    let endTime = document.querySelector("#timeEnd").value;
-    let color = document.querySelector("#color").value;
-    let obj = {
-      start: startTime,
-      duration: endTime - startTime,
-      title: text || "No text",
-      bgc: color,
-    };
-    events.push(obj);
-    initEvents(events);
+    addFormHandler();
   }
   if (e.target.name === "delete") {
-    let option = Array.from(remove.options).filter((item) => item.selected);
-    if (option.length >= 1) {
-      option.forEach((item) => events.splice(item.value, 1));
-    }
-    initEvents(events);
+    deleteFormHandler();
+  }
+  if (e.target.name === "change") {
+    changeFormHanlder();
   }
 }
 
+function addFormHandler() {
+  let text = document.querySelector("#event-title").value;
+  let startTime = +document.querySelector("#timeStart").value;
+  let endTime = +document.querySelector("#timeEnd").value;
+  let color = document.querySelector("#color").value;
+  let obj = {
+    start: +startTime,
+    duration: endTime < startTime ? 0 : endTime - startTime,
+    title: text || "No text",
+    bgc: color,
+  };
+  events.push(obj);
+  initEvents(events);
+}
+
+function deleteFormHandler() {
+  let option = remove.value;
+  if (option) {
+    events.splice(option, 1);
+    initEvents(events);
+  } else {
+    throw new Error("Choose come event for delete");
+  }
+}
+
+function changeFormHanlder() {
+  let selectedValue = replace.value;
+  let changedTitle = document.querySelector("#changeTitle");
+  let changedStart = document.querySelector("#changedStart");
+  let changedEnd = document.querySelector("#changedEnd");
+  events[selectedValue].title = changedTitle.value;
+  events[selectedValue].start = +changedStart.value;
+  events[selectedValue].duration = changedEnd.value - changedStart.value;
+  initEvents(events);
+  fillChangeForm();
+}
+
+function fillChangeForm() {
+  let selectedValue = replace.value;
+  if (selectedValue) {
+    let searchItem = events[selectedValue];
+    let { startTime, endTime } = changeFormTime(
+      searchItem.start,
+      searchItem.duration
+    );
+    let changedTitle = document.querySelector("#changeTitle");
+    changedTitle.value = searchItem.title;
+    let changedStart = document.querySelector("#changedStart");
+    let changedStartCounter = document.querySelector("#changedStart~span");
+    changedStart.value = searchItem.start;
+    changedStartCounter.innerHTML = startTime;
+    let changedEnd = document.querySelector("#changedEnd");
+    let changedEndCounter = document.querySelector("#changedEnd~span");
+    changedEnd.value = searchItem.start + searchItem.duration;
+    changedEndCounter.innerHTML = endTime;
+  }
+}
+
+function changeFormTime(start, end) {
+  let startTime =
+    `${Math.trunc(start / 60 + 8)}`.padStart(2, "0") +
+    ":" +
+    `${Math.round(start % 60)}`.padStart(2, "0");
+
+  let endTime =
+    `${Math.trunc((start + end) / 60 + 8)}`.padStart(2, "0") +
+    ":" +
+    `${Math.round((start + end) % 60)}`.padStart(2, "0");
+  return { startTime, endTime };
+}
+
+function showTimeHandler(e) {
+  let timePlace = document.querySelector(`#${e.target.id}~span`);
+  timePlace.innerHTML =
+    `${Math.trunc(e.target.value / 60 + 8)}`.padStart(2, "0") +
+    ":" +
+    `${Math.round(e.target.value % 60)}`.padStart(2, "0");
+}
+
+function comingEvent() {
+  let interval = setInterval(() => {
+    let date = `${new Date().getHours()}:${new Date().getMinutes()}`.split(":");
+    let eventsForShow = events.filter((event) => {
+      if (
+        Math.trunc(event.start / 60 + 8) === +date[0] - 1 &&
+        Math.round(event.start % 60) === +date[1]
+      ) {
+        return true;
+      }
+    });
+    renderPopUp(eventsForShow);
+  }, 30000);
+}
+
+function renderPopUp(eventsForModal) {
+  let wrapper = document.querySelector(".modal");
+  wrapper.innerHTML = "";
+  eventsForModal.forEach((event) => {
+    let modalHtml = `  <div class="modal__wrapper">
+                        <div class="modal__line"></div>
+                     <div class="modal__content">
+                      <p class="modal__text">
+                      The "${event.title}" event has started</p>
+                     </div>
+                     </div>`;
+    wrapper.insertAdjacentHTML("beforeend", modalHtml);
+  });
+  let showedPopup = document.querySelectorAll(".modal__wrapper");
+  showedPopup.forEach((popup) => {
+    popup.classList.toggle("active");
+  });
+  setTimeout(() => {
+    showedPopup.forEach((popup) => {
+      popup.classList.toggle("active");
+    });
+  }, 4500);
+}
 document.addEventListener("submit", formsHandler);
 
-document.querySelector("#timeStart").oninput = (e) => {
-  document.querySelector(".hours").innerHTML =
-    `${Math.trunc(e.target.value / 60 + 8)}`.padStart(2, "0") +
-    ":" +
-    `${Math.round(e.target.value % 60)}`.padStart(2, "0");
-};
+document.addEventListener("change", (e) => {
+  if (e.target.closest("form[name=change]") && e.target.tagName === "SELECT") {
+    fillChangeForm();
+  }
+});
 
-document.querySelector("#timeEnd").oninput = (e) => {
-  document.querySelector(".minutes").innerHTML =
-    `${Math.trunc(e.target.value / 60 + 8)}`.padStart(2, "0") +
-    ":" +
-    `${Math.round(e.target.value % 60)}`.padStart(2, "0");
-};
+document.querySelector("#timeStart").oninput = showTimeHandler;
+document.querySelector("#changedStart").oninput = showTimeHandler;
 
+document.querySelector("#timeEnd").oninput = showTimeHandler;
+document.querySelector("#changedEnd").oninput = showTimeHandler;
+
+addPropsToArr();
 initCalendar(start, end);
 initEvents(events);
+fillChangeForm();
+comingEvent();
